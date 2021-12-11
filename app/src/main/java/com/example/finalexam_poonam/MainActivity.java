140626f1acbff73d5ca6cee57b4ce0351124f40b
Poonam <poonammahi379@gmail.com>
2021-12-10 19:40:46 -0500

    functionality added

--- app/src/main/java/com/example/finalexam_poonam/MainActivity.java
+++ app/src/main/java/com/example/finalexam_poonam/MainActivity.java
@@ -12,58 +12,77 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationBarView;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    int selectedDie;
+    TextView result;
+    int selectedValue;
+    EditText customDie;
+    List<Integer> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView result= findViewById(R.id.result);
+        result= findViewById(R.id.result);
         Spinner dieType = findViewById(R.id.dieType);
         Button firstRoll = findViewById(R.id.firstRoll);
         Button secondRoll = findViewById(R.id.secondRoll);
-        EditText customdie= findViewById(R.id.customDie);
+        customDie= findViewById(R.id.customDie);
         Button addDie = findViewById(R.id.addDie);
-        List<Integer> list;
+
 
         firstRoll.setOnClickListener(this);
         secondRoll.setOnClickListener(this);
         addDie.setOnClickListener(this);
 
         list=new ArrayList<Integer>();
-        list.add(1);
         list.add(2);
+        list.add(4);
+
 
         ArrayAdapter<Integer> adp=new ArrayAdapter<Integer>(this,
                 android.R.layout.simple_dropdown_item_1line, list);
         adp.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         dieType.setAdapter(adp);
-        dieType.setSelection((list.size()-1));
 
         dieType.setOnItemSelectedListener(new OnItemSelectedListener(){
 
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,int arg2, long arg3) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
                 dieType.setSelection(arg2);
+                selectedDie=  list.get(arg2);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
             }
         });
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.firstRoll:
+                //Log.d("abc", String.valueOf(selectedDie));
+                selectedValue = (int)(Math.random() * selectedDie) + 1;
+                result.setText("Result of first roll: "+ String.valueOf(selectedValue));
+                break;
 
+            case R.id.secondRoll:
+                //Log.d("abc", String.valueOf(selectedDie));
+                selectedValue= (int)(Math.random() * selectedDie) + 1;
+                result.setText("Result of first roll: "+ String.valueOf(selectedValue)+ " " + "Result of second roll: "+ String.valueOf(selectedValue));
+                break;
 
+            case R.id.addDie:
+                int getDie= Integer.valueOf(String.valueOf(customDie.getText()));
+                list.add(getDie);
+        }
     }
 }
